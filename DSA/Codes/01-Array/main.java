1382a731c123f99929f1783ee59143eac0a63a33
RZA <77061473+rzathere@users.noreply.github.com>
2023-08-04 19:15:37 +0530

    Create main.java

---
+++ DSA/Codes/01-Array/main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // Q: store a roll number
+        int a = 19;
+
+        // Q: store a person's name
+        String name = "Masoom Raza";
+
+        // Q: store 5 roll numbers
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+//        int rno4 = 23;
+//        int rno5 = 23;
+
+        // syntax
+        // datatype[] variable_name = new datatype[size];
+        // store 5 roll numbers;
+        int[] rnos = new int[5];
+        // or directly
+        int[] rnos2 = {23, 12, 45, 32, 15};
+//        System.out.println(rnos);
+        int[] ros;
+        ros = new int[5];
+        char[] ros1;
+        ros1 = new char[5];
+        System.out.println(ros1[2]);
+
+        System.out.println(ros[2]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+
+    }
+}
